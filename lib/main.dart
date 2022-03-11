e6a3351f23cf3e84df3aaf3e5040f190e25a4beb
Philipp Schäfer <ph.schaefer@protonmail.com>
2022-03-12 00:33:31 +0100

    Styling & Theming the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
+          colorScheme: const ColorScheme.light(
+            primary: Colors.teal,
+            secondary: Colors.grey,
+          ),
+          fontFamily: 'Lato'),
       home: ProductsOverviewScreen(),
     );
   }
